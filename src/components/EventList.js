59d5d45ceffd72a7dadf97d31664650f594d755e
kimbyeongmo <kbmomo9999@naver.com>
2024-07-10 10:03:17 +0900

    # 초기 세팅

---
+++ src/components/EventList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './EventList.module.scss';
+import { Link } from 'react-router-dom';
+
+const EventList = ({ eventList }) => {
+
+  // loader 데이터는 loader를 선언한 페이지 밑에 있는 하위컴포넌트 어디서든 사용가능
+  // const data = useLoaderData();
+  // console.log('loader data : ', data);
+  
+
+  const {events, list, item, content} = styles;
+
+  return (
+    <div className={events}>
+      <h1>All Events</h1>
+      <ul className={list}>
+        {
+          eventList.map(ev => (
+            <li key={ev.id} className={item}>
+              <Link to={ev.id}>
+                <img src={ev['img-url']} alt={ev.title} />
+                <div className={content}>
+                  <h2>{ev.title}</h2>
+                  <time>{ev.startDate}</time>
+                </div>
+              </Link>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default EventList
---
+++ src/pages/NewEvent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import EventForm from '../components/EventForm';
+
+const NewEvent = () => {
+
+  return <EventForm method="post" />;
+};
+
+export default NewEvent;
