15217f2688f130ef2b67ee64fb5c9547585547f7
kimbyeongmo <kbmomo9999@naver.com>
2024-07-11 11:14:38 +0900

    # host config 설정

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState, useRef } from "react";
-import EventList from "../components/EventList";
-import EventSkeleton from "../components/EventSkeleton";
+import React, { useEffect, useState, useRef } from 'react';
+import EventList from '../components/EventList';
+import EventSkeleton from '../components/EventSkeleton';
+import { EVENT_URL } from '../config/host-config';
 
 // npm install loadsh
-import { debounce, throttle } from "lodash";
+// import { debounce, throttle } from 'lodash';
 
 const Events = () => {
+
   // loader가 리턴한 데이터 받아오기
   // const eventList = useLoaderData();
   // console.log(eventList);
@@ -13,13 +15,14 @@ const Events = () => {
   // 이벤트 목록 아래 박스 참조
   const skeletonBoxRef = useRef();
 
+
   // 서버에서 가져온 이벤트 목록
   const [events, setEvents] = useState([]);
 
   // 로딩 상태 체크
   const [loading, setLoading] = useState(false);
 
-  // 현재 페이지 번호
+  // 현재 페이지 번호 
   const [currentPage, setCurrentPage] = useState(1);
 
   // 더이상 가져올 데이터가 있는지 확인
@@ -28,35 +31,31 @@ const Events = () => {
   // 로딩 스켈레톤 스크린을 보여줄 개수
   const [skeletonCount, setSkeletonCount] = useState(4);
 
+
   // 서버로 목록 조회 요청보내기
-  const loadEvents = async () => {
+  const loadEvents = async() => {
+
     if (isFinish) {
-      console.log("loading finished!");
+      console.log('loading finished!');
       return;
     }
 
-    console.log("start loading...");
+    console.log('start loading...');
     setLoading(true);
 
-    const response = await fetch(
-      `http://localhost:8282/events/page/${currentPage}?sort=date`
-    );
+    const response = await fetch(`${EVENT_URL}/page/${currentPage}?sort=date`);
     const { events: loadedEvents, totalCount } = await response.json();
 
-    console.log("loaded: ", {
-      loadedEvents,
-      totalCount,
-      len: loadedEvents.length,
-    });
+    console.log('loaded: ', { loadedEvents, totalCount, len: loadedEvents.length });
 
     // console.log('loaded: ', loadedEvents);
 
-    const updatedEvents = [...events, ...loadedEvents];
+    const updatedEvents = [...events, ...loadedEvents ];
     setEvents(updatedEvents);
     setLoading(false);
     // 로딩이 끝나면 페이지번호를 1 늘려놓는다.
-    setCurrentPage((prevPage) => prevPage + 1);
-    console.log("end loading!!");
+    setCurrentPage(prevPage => prevPage + 1);
+    console.log('end loading!!');
 
     // 로딩이 끝나면 더 이상 가져올게 있는지 여부를 체크한다.
     setIsFinish(totalCount === updatedEvents.length);
@@ -67,6 +66,7 @@ const Events = () => {
     // skeleton 개수 구하기 -> 남은 개수가 4보다 크면 4로 세팅 4보다 작으면 그 수로 세팅
     const skeletonCnt = Math.min(4, restEventsCount);
     setSkeletonCount(skeletonCnt);
+
   };
 
   // 초기 이벤트 1페이지 목록 가져오기
@@ -76,7 +76,7 @@ const Events = () => {
 
   // 스크롤 핸들러
   // const scrollHandler = throttle(() => {
-  //   if (loading ||
+  //   if (loading || 
   //     window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight
   //   ) {
   //     return;
@@ -94,21 +94,26 @@ const Events = () => {
   //   }
   // }, [currentPage, loading]);
 
+
   // 화면에 특정 박스가 보이면 다음 페이지를 로딩
   useEffect(() => {
+
     const observer = new IntersectionObserver((entries) => {
-      // 현재 감시하고 있는 타겟의 정보(배열)
-      // console.log('entries: ', entries);
+
+      // 현재 감시하고 있는 타겟의 정보 (배열)
+      // console.log('entries: ', entries[0]);
 
       // 서버에서 데이터 페칭
-      // !entries[0].isIntersecting : 관찰하고 있는 박스가 감지되고 있지 않으면
+      // !entries[0].isIntersecting : 관찰하고 있는 박스가 감지되지 않으면
       if (!entries[0].isIntersecting || loading || isFinish) {
         return;
       }
+
       loadEvents();
-    },{
-      // 관찰하고 있는 요소의 높이가 50% 보일 때 콜백을 실행
-      threshold : 0.5
+
+    }, {
+      // 관찰하고 있는 요소의 높이가 50% 보일때 콜백을 실행
+      threshold: 0.5 
     });
 
     // observer 관찰 대상(DOM)을 지정
@@ -122,14 +127,16 @@ const Events = () => {
         observer.disconnect();
       }
     };
+
   }, [loading, currentPage]);
+
 
   return (
     <>
       <EventList eventList={events} />
-      <div
-        ref={skeletonBoxRef}
-        // style={{ height: "300px", background: "yellow" }}
+      <div 
+        ref={skeletonBoxRef} 
+        // style={{ height: '300px', background: 'yellow' }}
       >
         {loading && <EventSkeleton count={skeletonCount} />}
       </div>
@@ -143,7 +150,7 @@ export default Events;
 // export const loader = async () => {
 
 //   const response = await fetch('http://localhost:8282/events/page/1?sort=date');
-
+  
 //   if (!response.ok) {
 //     const errorText = await response.text();
 //     throw json(
@@ -152,6 +159,6 @@ export default Events;
 //         status: response.status
 //       }
 //     );
-//   }
+//   } 
 //   return response; // ok일 경우 events[]
 // };
