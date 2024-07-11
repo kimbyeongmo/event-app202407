0f0082e6fd55d3b378021b2810ec842f0e704191
kimbyeongmo <kbmomo9999@naver.com>
2024-07-11 12:39:04 +0900

    # 이메일 중복확인 요청

--- src/components/auth/EmailInput.js
+++ src/components/auth/EmailInput.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './SignUpForm.module.scss';
+import {AUTH_URL } from '../../config/host-config';
 
 const EmailInput = () => {
 
@@ -22,14 +23,23 @@ const EmailInput = () => {
   };
 
   // 이메일 검증 후속 처리
-  const checkEmail = (email) => {
+  const checkEmail = async() => {
     if (!emailVaild) {
       // 에러메시지 세팅
       setError('이메일 형식이 유효하지 않습니다.');
       return;
     }
 
-    // 중복 검사
+    // 중복 검사\
+      const response = await fetch(`${AUTH_URL}/check-email?email=${enteredEmail}`);
+      // console.log(`res : `,response);
+
+      const flag = await response.json();
+      console.log(flag);
+      if(flag){
+        setEmailValid(false);
+        setError('이메일이 중복되었습니다.');
+      }
   };
 
   const changeHandler = e => {
@@ -41,7 +51,7 @@ const EmailInput = () => {
     setEmailValid(isVaild);
 
     // 이메일 검증 후속처리
-    checkEmail();
+    checkEmail(email);
   };
 
   // 렌더링 되자마자 입력창에 포커싱
