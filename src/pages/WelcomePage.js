24bb6edfd88ffbd16f96e69f0c6437be6e752af4
kimbyeongmo <kbmomo9999@naver.com>
2024-07-16 09:57:29 +0900

    # 로그인 조건부 렌더링

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,16 @@
-import React from 'react';
 import LoginForm from '../components/auth/LoginForm';
+import Main from '../components/Main';
 
 const WelcomePage = () => {
-  return <LoginForm />
+
+  console.log('WelcomePage 실행!');
+
+  return (
+    <>
+      {<LoginForm />}
+      {<Main />}
+    </>
+  );
 };
 
 export default WelcomePage;
